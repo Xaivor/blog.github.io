54a32c777e783e782f2808b7b93c3c7be442fad6
Xulei <lxu@justhealth.cn>
2024-04-16 16:41:54 +0800

    'chore:deploy'

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,14 +1,14 @@
 const shelljs = require('shelljs');
 
 
-// shelljs.exec(`npm run docs:build`)
-// shelljs.exec(`echo 'Pack is building Success..., is deploying...'`);
+shelljs.exec(`npm run docs:build`)
+shelljs.exec(`echo 'Pack is building Success..., is deploying...'`);
 
 shelljs.exec(`cd docs/.vitepress/dist`)
-shelljs.exec(`ls`)
-shelljs.exec(`git init`)
-shelljs.exec(`git add -A`)
-shelljs.exec(`git commit -m 'chore:deploy'`)
+shelljs.exec(`cd docs/.vitepress/dist && git init && git add -A && git commit -m 'chore:deploy' && git push -f https://github.com/Xaivor/blog.git master:docs`)
 
-// shelljs.exec(`git push -f https://github.com/Xaivor/blog.git master:docs`)
-// shelljs.exec(`echo 'Deploy Success...'`);
+shelljs.exec(`echo 'Deploy Docs Success...'`);
+
+shelljs.exec(`git init && git add -A && git commit -m 'chore:deploy' && git push -f https://github.com/Xaivor/blog.git master`)
+
+shelljs.exec(`echo 'Deploy Program Success...'`);
