f9163458fa670deba9ffcd4ddd901b0a0797222e
Xulei <lxu@justhealth.cn>
2024-04-16 16:37:11 +0800

    'chore:deploy'

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,13 +1,14 @@
 const shelljs = require('shelljs');
 
 
-shelljs.exec(`npm run docs:build`)
-shelljs.exec(`echo 'Pack is building Success..., is deploying...'`);
+// shelljs.exec(`npm run docs:build`)
+// shelljs.exec(`echo 'Pack is building Success..., is deploying...'`);
 
 shelljs.exec(`cd docs/.vuepress/dist`)
+shelljs.exec(`ls`)
 shelljs.exec(`git init`)
 shelljs.exec(`git add -A`)
 shelljs.exec(`git commit -m 'chore:deploy'`)
 
-shelljs.exec(`git push -f https://github.com/Xaivor/blog.git master`)
-shelljs.exec(`echo 'Deploy Success...'`);
+// shelljs.exec(`git push -f https://github.com/Xaivor/blog.git master:docs`)
+// shelljs.exec(`echo 'Deploy Success...'`);
