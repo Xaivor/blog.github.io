0bee29aaccb13ab148068fb99cbba6beb9ec120f
Xulei <lxu@justhealth.cn>
2024-04-19 18:00:36 +0800

    fix: 修改

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Xaivor 个人博客
-> 路吗漫其修远兮~
+> 路漫漫其修远兮~
 
 点我预览：[个人在线博客地址](https://xaivor.github.io/blog/)
--- bin/deploy.js
+++ bin/deploy.js
@@ -1,18 +1,24 @@
 const shelljs = require('shelljs');
 
+// 文档打包路径
+const distPath = 'docs/.vitepress/dist';
+
+// github 地址
+const githubUrl = 'https://github.com/Xaivor/blog.git';
+
 // 打包
 shelljs.exec(`npm run docs:build`)
 shelljs.exec(`echo 'Pack is building Success..., is deploying...'`);
 
 // 拷贝自动构建文件
-shelljs.mkdir('docs/.vitepress/dist/.github')
-shelljs.mkdir('docs/.vitepress/dist/.github/workflows')
-shelljs.cp('bin/static.yml', 'docs/.vitepress/dist/.github/workflows')
+shelljs.mkdir(`${distPath}/.github`)
+shelljs.mkdir(`${distPath}/.github/workflows`)
+shelljs.cp('bin/static.yml', `${distPath}/.github/workflows`)
 
 // 提交到github
-shelljs.exec(`cd docs/.vitepress/dist && git init && git add -A && git commit -m 'chore:deploy' && git push -f https://github.com/Xaivor/blog.git master:docs`)
+shelljs.exec(`cd docs-dist && git init && git add -A && git commit -m 'chore:deploy' && git push -f ${githubUrl} master:docs`)
 shelljs.exec(`echo 'Deploy Docs Success...'`);
 
 // 删除dist文件夹
-shelljs.rm('-rf', 'docs/.vitepress/dist');
+shelljs.rm('-rf', distPath);
 shelljs.exec(`echo 'Delete dist Success...'`);
